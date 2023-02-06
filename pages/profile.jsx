f4e0f863dcf916776d628fdd5cefac0457d07956
madomaga <marcin.domagala@capgemini.com>
2023-02-06 15:48:26 +0100

    Prepare profile screen

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, SafeAreaView, ScrollView } from 'react-native';
+import { Avatar, List } from 'react-native-paper';
 
-export default function ProfileScreen({navigation, route}) {
+export default function ProfileScreen({ navigation, route }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>This is profile</Text>
-      <Button onPress={() => navigation.navigate('HomeScreen')} title="Home"/>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <List.Section>
+          <List.Subheader>Profile</List.Subheader>
+          <List.Item title="Your name" />
+          <List.Item title="Your surname" />
+          <List.Item title="Your location" />
+          <List.Item
+            title="Your image"
+            right={() => <Avatar.Image size={32} source={require('../assets/avatar.png')} />}
+          />
+        </List.Section>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
